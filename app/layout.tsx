5e0978705ea7537a243e35696e3b8baf38c2af11
Hashim332 <ppyhr5@nottingham.ac.uk>
2025-09-03 17:39:52 +0300

    setup login component and sign-in/sign-up pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,30 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata = {
-  metadataBase: new URL('https://postgres-drizzle.vercel.app'),
-  title: 'Postgres Demo with Drizzle',
+  metadataBase: new URL("https://postgres-drizzle.vercel.app"),
+  title: "Postgres Demo with Drizzle",
   description:
-    'A simple Next.js app with a Postgres database and Drizzle as the ORM',
-}
+    "A simple Next.js app with a Postgres database and Drizzle as the ORM",
+};
 
 const inter = Inter({
-  variable: '--font-inter',
-  subsets: ['latin'],
-  display: 'swap',
-})
+  variable: "--font-inter",
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.variable}>{children}</body>
-    </html>
-  )
+    <ClerkProvider>
+      <html lang='en'>
+        <body className={inter.variable}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
 }
